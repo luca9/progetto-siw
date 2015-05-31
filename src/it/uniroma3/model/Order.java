169b2c47517a433c50e6809d5d1c4efff0fd474b
lucabellisai <lucabellisai@10.25.207.92>
2015-05-31 22:19:01 +0200

    modifiche per il caso d'uso2

--- src/it/uniroma3/model/Order.java
+++ src/it/uniroma3/model/Order.java
@@ -1,11 +1,22 @@
 package it.uniroma3.model;
 
-import javax.persistence.*;
-import java.util.ArrayList;
+import it.uniroma3.enums.OrderState;
+
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-import it.uniroma3.enums.OrderState;
+import java.util.Map;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 /**
  * Created by lorenzovalente on 27/03/15.
@@ -15,93 +26,99 @@ import it.uniroma3.enums.OrderState;
 @Table(name = "CustomerOrder")
 public class Order {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
-    @Column(nullable = false)
-    private Date creationTime;
+	@Column(nullable = false)
+	private Date creationTime;
 
-    private OrderState orderState;
+	private OrderState orderState;
 
-    @ManyToOne
-    private RegisteredUser customer;
+	@ManyToOne
+	private RegisteredUser customer;
 
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
-    private List<OrderLine> orderLines;
-
-
-    public Order() {
-        this.creationTime = Calendar.getInstance().getTime();
-    }
-
-    public Order (RegisteredUser customer) {
-        this.creationTime = Calendar.getInstance().getTime();
-        this.customer = customer;
-        this.orderLines = new ArrayList<OrderLine>();
-    }
-
-    public Long getId () {
-        return id;
-    }
-
-    public Date getCreationTime() {
-        return creationTime;
-    }
-
-    public OrderState getOrderState() {
-        return orderState;
-    }
-
-    public void setOrderState(OrderState orderState) {
-        this.orderState = orderState;
-    }
-
-    public void placeOrder () {
-        this.setOrderState(OrderState.PLACED);
-    }
-
-    public void dispatchOrder () {
-        this.setOrderState(OrderState.DISPATCHED);
-    }
-
-    public User getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(RegisteredUser customer) {
-        this.customer = customer;
-    }
-
-    public void setUser(User user) {
-        this.customer = customer;
-    }
-
-    public List<OrderLine> getOrderLines() {
-        return orderLines;
-    }
-
-    public void setOrderLines(List<OrderLine> orderLines) {
-        this.orderLines = orderLines;
-    }
-
-    public void addOrderLine (OrderLine orderLine) {this.orderLines.add(orderLine);}
+	@OneToMany
+	@JoinColumn(name = "orders_id")
+	private Map<Long, OrderLine> orderLines;
 
 
-    @Override
-    public boolean equals(Object o) {
-        Order order = (Order) o;
-        return this.getId() == order.getId();
-    }
+	public Order() {
+		this.creationTime = Calendar.getInstance().getTime();
+	}
+
+	public Order (RegisteredUser customer) {
+		this.creationTime = Calendar.getInstance().getTime();
+		this.customer = customer;
+		this.orderLines = new HashMap<Long,OrderLine>();
+	}
+
+	public Long getId () {
+		return id;
+	}
+
+	public Date getCreationTime() {
+		return creationTime;
+	}
+
+	public OrderState getOrderState() {
+		return orderState;
+	}
+
+	public void setOrderState(OrderState orderState) {
+		this.orderState = orderState;
+	}
+
+	public void placeOrder () {
+		this.setOrderState(OrderState.PLACED);
+	}
+
+	public void dispatchOrder () {
+		this.setOrderState(OrderState.DISPATCHED);
+	}
+
+	public User getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(RegisteredUser customer) {
+		this.customer = customer;
+	}
+
+	public void setUser(User user) {
+		this.customer = customer;
+	}
+
+	public Map<Long, OrderLine> getOrderLines() {
+		return orderLines;
+	}
+
+	public void setOrderLines(Map<Long, OrderLine> orderLines) {
+		this.orderLines = orderLines;
+	}
+	
+	public void addProductToOrder(int quantity, Product product) throws Exception {
+		OrderLine a = this.orderLines.get(product.getId());
+		if (a==null) {
+			a = orderLines.put(product.getId(),new OrderLine(product.getPrice(),0,product));
+		}
+		a.setQuantity(a.getQuantity() + quantity);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		Order order = (Order) o;
+		return this.getId() == order.getId();
+	}
 
 
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", creationTime=" + creationTime +
-                ", customer=" + customer.getLastName() +
-                ", orderLines=" + orderLines +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Order{" +
+				"id=" + id +
+				", creationTime=" + creationTime +
+				", customer=" + customer.getLastName() +
+				", orderLines=" + orderLines +
+				'}';
+	}
 }
