31a91a9615a061572fcbeccb2f9d94f0cb0c6559
lucabellisai <lucabellisai@yahoo.it>
2015-06-15 10:26:48 +0200

    Modifiche

--- src/it/uniroma3/validator/IDValidator.java
+++ src/it/uniroma3/validator/IDValidator.java
@@ -1,13 +1,13 @@
 package it.uniroma3.validator;
 
+import it.uniroma3.facade.ProductFacade;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
-
-import it.uniroma3.facade.ProductFacade;
 
 /**
  * Created by lorenzovalente on 23/05/15.
